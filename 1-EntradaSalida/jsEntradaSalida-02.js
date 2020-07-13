b68ba7c5436edf1feb894daa29c48781f564681f
martinrolon39 <martin.rolon39@gmail.com>
2020-07-13 23:13:57 +0200

    ejersicios hasta el 7

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,20 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+
+	// var nombre; // camel case
+
+	//var nombreCompleto; // undefined
+	//var nombre = "mario"; //String 
+	//var nombre = 1; // number 
+	//var nombre = true; // boolean 
+	//ver nombre = null; // null
+var dato;
+
+dato = prompt ("ingrese un dato");
+
+alert (dato);
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,11 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var dato;
+
+	dato = prompt ("ingrese su nombre");
+	
+	txtIdNombre.value = dato;
+
 }
 
